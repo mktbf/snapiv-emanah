46b8191dcb78d4741e1bb240cb10525ab2980690
Comunicação e Marketing - BF <grupobomfuturo.mt@gmail.com>
2026-05-28 20:39:15 -0400

    Add Gmail as test recipient

--- api/submit.js
+++ api/submit.js
@@ -49,7 +49,7 @@ module.exports = async function handler(req, res) {
 
     const payload = {
       from: 'SNAP-IV Emanah <noreply@bomfuturo.com.br>',
-      to: ['dani_lbc@hotmail.com', 'jean_toya@hotmail.com'],
+      to: ['dani_lbc@hotmail.com', 'jean_toya@hotmail.com', 'grupobomfuturo.mt@gmail.com'],
       subject: `SNAP-IV - ${nome} - ${data}`,
       html: htmlBody,
     };
