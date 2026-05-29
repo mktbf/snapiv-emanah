4dab44c15c1d9fc30e7b50bf02c54b0b0266affb
Comunicação e Marketing - BF <grupobomfuturo.mt@gmail.com>
2026-05-28 20:10:42 -0400

    Use bomfuturo.com.br as sender domain

--- api/submit.js
+++ api/submit.js
@@ -48,7 +48,7 @@ module.exports = async function handler(req, res) {
       </div>`;
 
     const payload = {
-      from: 'SNAP-IV Emanah <onboarding@resend.dev>',
+      from: 'SNAP-IV Emanah <noreply@bomfuturo.com.br>',
       to: ['dani_lbc@hotmail.com', 'jean_toya@hotmail.com'],
       subject: `SNAP-IV - ${nome} - ${data}`,
       html: htmlBody,
